143458c16fecf305543a56fb266d9449ba864513
Cavin Dsouza <cavin46016@gmail.com>
2017-05-06 23:56:31 -0400

    Update userIDList.py

--- userIDList.py
+++ userIDList.py
@@ -1,15 +1,10 @@
 import json
 
 """ Business yelp dataset file """
-SOURCE = 'D:\\New folder\\yelp_academic_dataset_review_restaurants.json';
+SOURCE = 'yelp_academic_dataset_review_restaurants.json';
 
 def findNWriteListToTxt():
-    """
-    catg ==> Category
-    Checks for "categories" column under business dataset and writes only the business id's
-    based on user entered category 'catg', into a text file.
-    """
-    txt_file = open("D:\\New folder\\userID.txt", "w")
+    txt_file = open("userID.txt", "w")
     with open(SOURCE, encoding='utf-8', errors = 'replace') as f:
         for line in f:
             data = json.loads(line); 
