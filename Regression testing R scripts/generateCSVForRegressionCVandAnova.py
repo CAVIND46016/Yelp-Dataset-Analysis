489e17f7916c52c55d0143e79f7d838cbd4bb8fe
Cavin Dsouza <cavin46016@gmail.com>
2017-08-03 04:45:52 -0400

    Update generateCSVForRegressionCVandAnova.py

--- Regression testing R scripts/generateCSVForRegressionCVandAnova.py
+++ Regression testing R scripts/generateCSVForRegressionCVandAnova.py
@@ -5,8 +5,8 @@ from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 
-SOURCE = 'D:\\YELP Dataset\\yelp_academic_dataset_review.json';
-CSV_FILE = 'D:\\YELP Dataset\\yelp_regression.csv'
+SOURCE = 'yelp_academic_dataset_review_restaurants.json';
+CSV_FILE = 'yelp_regression.csv'
     
 """Remove non-ASCII chars."""
 def normalize_text(text):
@@ -22,9 +22,6 @@ def generateCSV(count = None):
     
     with open("negative-words.txt") as f:
         neg_words = f.read().split()[213:]
-
-    with open("D:\\YELP Dataset\\tobesubmitted\\restaurantID.txt", 'r') as busIds:
-        id_list = busIds.read();
             
     if(count != None):
         c = 0    
@@ -34,22 +31,21 @@ def generateCSV(count = None):
   
         with open(SOURCE, encoding = 'utf-8', errors = 'replace') as f:
             for line in f:
-                data = json.loads(line) 
-                if(data['business_id'] in id_list):    
-                    data['text'] = ''.join([normalize_text(text) for text in data['text']]) 
-                    word_list = tokenizer.tokenize(data['text'])
-                    filtered_words = [word for word in word_list if word not in stopwords.words('english')]
-                    stemmed_words = []
-                    for i in filtered_words:
-                        stemmed_words.append(stemmer.stem(i))
+                data = json.loads(line)  
+                data['text'] = ''.join([normalize_text(text) for text in data['text']]) 
+                word_list = tokenizer.tokenize(data['text'])
+                filtered_words = [word for word in word_list if word not in stopwords.words('english')]
+                stemmed_words = []
+                for i in filtered_words:
+                    stemmed_words.append(stemmer.stem(i))
 
-                    num_positive = sum([r in pos_words for r in stemmed_words])
-                    num_negative = sum([r in neg_words for r in stemmed_words])
-                    w.writerow([str(len(data['text'])), str(num_positive), str(num_negative), data['stars']])  
-                    if(count != None):
-                        c = c + 1
-                        if(c == count):
-                            break;
+                num_positive = sum([r in pos_words for r in stemmed_words])
+                num_negative = sum([r in neg_words for r in stemmed_words])
+                w.writerow([str(len(data['text'])), str(num_positive), str(num_negative), data['stars']])  
+                if(count != None):
+                    c = c + 1
+                    if(c == count):
+                        break;
                     
     print("{} generated successfully.".format(CSV_FILE))
     
