108afebc286a01fe45071b2d2c8a16b2218e5c0b
Cavin Dsouza <cavin46016@gmail.com>
2018-03-21 00:24:08 -0400

    Update wordcloud_generation.py

--- wordclouds/wordcloud_generation.py
+++ wordclouds/wordcloud_generation.py
@@ -1,25 +1,30 @@
-#!/usr/bin/env python
-"""https://github.com/amueller/word_cloud/blob/master/examples/simple.py"""
+"""
+https://github.com/amueller/word_cloud/blob/master/examples/simple.py
+"""
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-def generateWordCloud(text):
+def generate_wc(text):
     """
-    text ==> string to be considered for generating wordcloud
-    #***************************************************************************************
-    #*    Title: word_cloud
-    #*    Author: Andreas Mueller
-    #*    Availability: https://github.com/amueller/word_cloud/blob/master/examples/simple.py"
-    #***************************************************************************************
+    text - string to be considered for generating wordcloud
+    ***************************************************************************************
+    *    Title: word_cloud
+    *    Author: Andreas Mueller
+    *    Availability: https://github.com/amueller/word_cloud/blob/master/examples/simple.py"
+    ***************************************************************************************
     """
-    wc = WordCloud(max_font_size = 40).generate(text)
-    plt.imshow(wc, interpolation = "bilinear")
+    w_cloud = WordCloud(max_font_size=40).generate(text)
+    plt.imshow(w_cloud, interpolation="bilinear")
     plt.axis("off")
     plt.show()
 
 def main():
-    dataset = open("D:\\Yelp_1star_wc_sample_adj.csv", 'r').read();
-    generateWordCloud(dataset);
+    """
+    Entry-point for the function
+    """
+    data_set = open("yelp_1star_wc_sample_adj.csv", 'r').read()
+    generate_wc(data_set)
+    data_set.close()
     
-if(__name__ == "__main__"):
-    main();
+if __name__ == "__main__":
+    main()
