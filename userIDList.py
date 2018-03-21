3b02c6b08b200957795897bf02d481b0879b7b5d
Cavin Dsouza <cavin46016@gmail.com>
2018-03-21 00:02:21 -0400

    Update userIDList.py

--- userIDList.py
+++ userIDList.py
@@ -1,20 +1,30 @@
+"""
+Retrieve a list of business id's
+"""
 import json
 
-""" Business yelp dataset file """
-SOURCE = 'yelp_academic_dataset_review_restaurants.json';
-
-def findNWriteListToTxt():
-    txt_file = open("userID.txt", "w")
-    with open(SOURCE, encoding='utf-8', errors = 'replace') as f:
-        for line in f:
-            data = json.loads(line); 
-            txt_file.write("{}\n".format(data['user_id']));          
+def write_to_text(src_file, op_file):
+    """
+    Retreieve the list and write to text file.
+    """
+    txt_file = open(op_file, "w")
+    with open(src_file, encoding='utf-8', errors='replace') as j_file:
+        for line in j_file:
+            data = json.loads(line)
+            txt_file.write("{}\n".format(data['user_id']))         
                 
-    txt_file.close();
+    txt_file.close()
     print("List created successfully.")
 
 def main():
-    findNWriteListToTxt();
+    """
+    Entry-point for the main function.
+    """
+    src_file = 'yelp_academic_dataset_review_restaurants.json'
+    op_file = 'user_id.txt'
+    
+    write_to_text(src_file, op_file)
 
-if(__name__ == "__main__"):
-    main();
+if __name__ == "__main__":
+    main()
+    
